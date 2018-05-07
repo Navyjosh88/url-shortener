bb0a79f3cea84378a476853c35fc046d277d6264
Josh Vargas <navyjosh88@gmail.com>
2018-05-07 16:51:28 -0400

    initial commit

---
+++ app/models/url.rb
@@ -0,0 +1,11 @@
+class Url < ApplicationRecord
+  validates :short, uniqueness: true
+  before_validation :generate, on: :create
+
+  def generate
+    self.short = SecureRandom.hex(3)
+    while Url.where(short: self.short).exists?
+      self.short = ('a'..'z').to_a.shuffle.take(1).join + SecureRandom.hex(2) + ('a'..'z').to_a.shuffle.take(1).join
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  resources :url
+end
